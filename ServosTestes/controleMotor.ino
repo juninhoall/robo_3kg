c1fbb5d104366d070a56e0b4900d274444b80f99
Osmar Petry <osmarpetry@gmail.com>
2016-06-24 16:00:24 -0300

    Código para acelerar progressivamente

--- ServosTestes/controleMotor.ino
+++ ServosTestes/controleMotor.ino
@@ -16,6 +16,17 @@ void frente(int potencia) {
   //delay(2000);
 
 }
+void acelerarFrente(int potenciaIni) {
+  if (potenciaIni < 40 || potenciaIni > 90) {
+    potenciaIni = 90;
+  }
+  do {
+    ST1.write(potenciaIni);
+    ST2.write(potenciaIni);
+    potenciaIni - 5;
+  }
+  while (potenciaIni > 40);
+}
 
 // pulso deve ser de 94 maximo 144 minimo
 void  tras(int potencia) {
@@ -24,20 +35,27 @@ void  tras(int potencia) {
   potenciaD = potencia;
   potenciaD;
 }
-
+void acelerarTras(int potenciaIni) {
+  if (potenciaIni > 94 || potenciaIni < 144) {
+    potenciaIni = 144;
+  }
+  do {
+    ST1.write(potenciaIni);
+    ST2.write(potenciaIni);
+    potenciaIni - 5;
+  }
+  while (potenciaIni > 94);
+}
 
 // girar no proprio eixo
 void direita() {
   ST1.write(90);
   ST2.write(95);
-
-
-
 }
 
 void esquerda() {
-ST1.write(92);
-ST2.write(potenciaD);
+  ST1.write(92);
+  ST2.write(potenciaD);
 
 }
 
@@ -48,13 +66,8 @@ void parar() {
 
 }
 
-void acelerarFrente() {
 
-}
 
-void acelerarTras() {
-
-}
 /*
    Regras de pulso, para não estorar.
 */
