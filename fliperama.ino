6b59ad16226c7d854bb197f75c64479bae8376ff
luiz almeida junior <allmeidajr@gmail.com>
2016-07-01 23:31:29 -0300

    Contador

--- fliperama.ino
+++ fliperama.ino
@@ -1,9 +1,10 @@
 
 int sensorPin = A0;    // select the input pin for the potentiometer
-//int ledPin = 13;      // select the pin for the LED
+int sensorPin2 = A1;
 int sensorValue = 0;  // variable to store the value coming from the sensor
-
+int  sensorValue2 = 0;
 static unsigned long ult_tempo = 0;
+boolean start = false;
 
 long seg = 0,
      minu = 0,
@@ -12,48 +13,62 @@ long seg = 0,
 
 void setup() {
   delay(500);
-  // declare the ledPin as an OUTPUT:
-  // pinMode(ledPin, OUTPUT);
   Serial.begin(9600);
 
 }
 
 void loop() {
-  long tempo = millis();
-  mil = tempo % 1000;
-  // read the value from the sensor:
-
+  start = false;
   sensorValue = analogRead(sensorPin);
-  // delay(2222);
-  if (sensorValue > 50) {
+Serial.println(sensorValue);
+  if (sensorValue > 30) {
+    start = true;
 
-    if (tempo - ult_tempo >= 1000 ) {
-      ult_tempo = tempo;
-      seg++;
-
+  }
+  while (start) {
+    comecar();
+    sensorValue2 = analogRead(sensorPin2);
+    Serial.println(sensorValue2);
+   // delay(1000);
+    if (sensorValue2 > 30) {
+      start = false;
+      Serial.end();
     }
 
-    if (seg >= 60) {
-      seg = 0;
-      minu++;
-    }
-    if (minu >= 60) {
-      minu = 0;
-      hor++;
-    }
-    if (hor >= 24) {
-      hor = 0;
-      minu = 0;
-    }
+  }
+}
+void comecar() {
+  //sensorValue = analogRead(sensorPin);
+  cronometro();
+}
 
-    Serial.print(hor);
-    Serial.print(":");
-    Serial.print(minu);
-    Serial.print(":");
-    Serial.print(seg);
-    Serial.print(":");
-    Serial.println(mil);
+void cronometro() {
+  long tempo = millis();
+
+  mil = tempo % 1000;
+  if (tempo - ult_tempo >= 1000 ) {
+    ult_tempo = tempo;
+    seg++;
+
   }
 
-
+  if (seg >= 60) {
+    seg = 0;
+    minu++;
+  }
+  if (minu >= 60) {
+    minu = 0;
+    hor++;
+  }
+  if (hor >= 24) {
+    hor = 0;
+    minu = 0;
+  }
+  Serial.print(hor);
+  Serial.print(":");
+  Serial.print(minu);
+  Serial.print(":");
+  Serial.print(seg);
+  Serial.print(":");
+  Serial.println(mil);
 }
